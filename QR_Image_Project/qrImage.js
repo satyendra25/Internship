3cb47294c5ae9f9053a158a5d3b49b6aa0e195f6
SATYENDRA SINGH BAGHEL <141430744+satyendra25@users.noreply.github.com>
2024-02-13 09:50:09 +0530

    qrImage.js

---
+++ QR_Image_Project/qrImage.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+import qr from 'qr-image';
+import fs from 'fs';
+
+
+inquirer
+.prompt([
+    {
+        message: " Enter your URL",
+        name: "URL",
+    },
+])
+.then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr-image.png"));
+    fs.writeFile("URL.txt", url, (err) => {
+        if(err) throw err;
+        console.log("The file saved");
+    });
+})
+    .catch((error) => {
+        if(error.isTtyError){
+
+        } else {
+
+        }
+    });
